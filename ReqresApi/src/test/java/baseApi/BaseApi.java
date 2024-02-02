290491329db004fd7cc0e8b48e2f97114c5c8529
Pom <62348850+Pom2012@users.noreply.github.com>
2024-02-02 17:34:51 -0500

    added BaseApi class

--- ReqresApi/src/test/java/baseApi/BaseApi.java
+++ ReqresApi/src/test/java/baseApi/BaseApi.java
@@ -1,4 +1,11 @@
 package baseApi;
 
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 public class BaseApi {
+
+    public static Response response;
+    public static RequestSpecification request;
+
 }
