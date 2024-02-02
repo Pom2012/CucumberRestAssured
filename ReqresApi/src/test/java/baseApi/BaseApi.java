aef1663bffdae5a3502bb842ad3f07b1a25b5685
Pom <62348850+Pom2012@users.noreply.github.com>
2024-02-02 18:25:15 -0500

    created BaseApi

--- ReqresApi/src/test/java/baseApi/BaseApi.java
+++ ReqresApi/src/test/java/baseApi/BaseApi.java
@@ -2,10 +2,17 @@ package baseApi;
 
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
 
 public class BaseApi {
 
-    public static Response response;
-    public static RequestSpecification request;
-
+    public static Response RESPONSE;
+    public static RequestSpecification REQUEST;
+    public static Logger log = LoggerFactory.getLogger(BaseApi.class);
+    public static List<List<String>> listValue = null;
+    public static List<Map<String,String>> mapList =null;
 }
