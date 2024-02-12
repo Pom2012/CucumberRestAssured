86063744905a4e51786668a16827489e65c03598
Pom <62348850+Pom2012@users.noreply.github.com>
2024-02-12 10:02:10 -0500

    updated BaseApi class

--- ReqresApi/src/test/java/baseApi/BaseApi.java
+++ ReqresApi/src/test/java/baseApi/BaseApi.java
@@ -2,17 +2,22 @@ package baseApi;
 
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 
-public class BaseApi {
+public abstract class BaseApi {
 
     public static Response RESPONSE;
     public static RequestSpecification REQUEST;
     public static Logger log = LoggerFactory.getLogger(BaseApi.class);
     public static List<List<String>> listValue = null;
     public static List<Map<String,String>> mapList =null;
+    public JSONObject jsonObject = new JSONObject();
+    File file;
+
 }
